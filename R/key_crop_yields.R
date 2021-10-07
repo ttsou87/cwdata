22a7dcba7499a14d327803f2e09c2bad84e0ee78
Tina Tsou <ttso0004@student.monash.edu>
2021-10-07 17:32:36 +0800

    add README add data documentations

---
+++ R/key_crop_yields.R
@@ -0,0 +1,19 @@
+# 'Title of the data
+#'
+#' More details about the data
+#'
+#' @section Background:
+#' The data was collected from ... over the course of ...
+#'
+#' @format: A data frame with 143,825 rows and 5 variables:
+#' \describe{
+#'    \item{entity}{name of the country}
+#'    ...
+
+#'}
+#'
+#' @source Where you got the data
+#'
+#' @examples
+#' print(key_crop_yields)
+"key_crop_yields"
