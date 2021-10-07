11f11c8f09a4293cba43424be62f606ea4836ec3
Tina Tsou <ttso0004@student.monash.edu>
2021-10-07 18:02:02 +0800

    update key_crop_yields.R and README.Rmd

--- R/key_crop_yields.R
+++ R/key_crop_yields.R
@@ -5,9 +5,13 @@
 #' @section Background:
 #' The data was collected from ... over the course of ...
 #'
-#' @format: A data frame with 143,825 rows and 5 variables:
+#' @format: A data frame with `r nrow(key_crop_yields)` rows and `r ncol(key_crop_yields)` variables:
 #' \describe{
 #'    \item{entity}{name of the country}
+#'    \item{code}{3 letter country code}
+#'    \item{year}{year from 1961 to 2018}
+#'    \item{crop}{crops included are `r unique(key_crop_yields$crop`}
+#'    \item{tonnes_per_hectare}{produce of crop in tonnes per hectare}
 #'    ...
 
 #'}
